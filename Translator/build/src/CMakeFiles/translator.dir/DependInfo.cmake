c8d4f17ce4a36498f06dd08ffe0891107cb263f1
Chris <chris.renschler@gmail.com>
2020-03-23 12:47:41 +0100

    Using CMake to generate Makefile

---
+++ Translator/build/src/CMakeFiles/translator.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/src/helpers.cpp" "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/build/src/CMakeFiles/translator.dir/helpers.cpp.o"
+  "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/src/lexer.yy.cpp" "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/build/src/CMakeFiles/translator.dir/lexer.yy.cpp.o"
+  "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/src/main.cpp" "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/build/src/CMakeFiles/translator.dir/main.cpp.o"
+  "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/src/parser.tab.cpp" "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/build/src/CMakeFiles/translator.dir/parser.tab.cpp.o"
+  "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/src/print_canonical.cpp" "/mnt/c/Users/adria/Desktop/Visual Studio/Translator/Translator/build/src/CMakeFiles/translator.dir/print_canonical.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Translator/build/src/CMakeFiles/translator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/translator.dir/helpers.cpp.o"
+  "CMakeFiles/translator.dir/lexer.yy.cpp.o"
+  "CMakeFiles/translator.dir/main.cpp.o"
+  "CMakeFiles/translator.dir/parser.tab.cpp.o"
+  "CMakeFiles/translator.dir/print_canonical.cpp.o"
+  "translator.pdb"
+  "translator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/translator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
