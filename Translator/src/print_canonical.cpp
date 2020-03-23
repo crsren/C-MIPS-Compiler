63d5bc3d77f7ad1031c627d098a271799f89daee
Chris <chris.renschler@gmail.com>
2020-03-23 12:47:59 +0100

    Remove second main

--- Translator/src/print_canonical.cpp
+++ Translator/src/print_canonical.cpp
@@ -1,11 +1,11 @@
-#include "../include/AST.h"
-
-int main()
-{
-    const Node *ast = parseAST();
-
-    ast->print(std::cout);
-    std::cout << std::endl;
-
-    return 0;
-}
+//#include "AST.h"
+//
+//int main()
+//{
+//    const Node *ast = parseAST();
+//
+//    ast->print(std::cout);
+//    std::cout << std::endl;
+//
+//    return 0;
+//}
