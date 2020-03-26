0093592f75cc8a5faa9adf2b297420cb76181fba
Chris <chris.renschler@gmail.com>
2020-03-26 23:34:46 +0100

    Mips helper structure

--- Compiler/include/mips.h
+++ Compiler/include/mips.h
@@ -1,20 +1,21 @@
 #if !defined(MIPS_H)
 #define MIPS_H
 
-// Functions with return type string that implement different MIPS instructions
-// to be called inside print functions of Nodes
+// MIPS Helper functions
+// - implement different MIPS instructions
+// - called inside print functions of Nodes
 
 #include <string>
 
 using namespace std;
 
-class Mips
+class Mips //call like this:  "Mips::move(...);"
 {
 public:
     string move(string r1, string r2)
     {
         return "\tmove\t" + r1 + "," + r2;
     };
-}
+};
 
 #endif // MIPS_H
